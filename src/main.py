57ded199a5dc6a61d9f43a547daf45128652c807
jbeardwo <jfwbeardwood@gmail.com>
2025-10-27 21:02:54 -0700

    added generate page function

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from markdown_to_html_node import markdown_to_html_node
 from textnode import TextNode, TextType
 import os
 import shutil
@@ -6,6 +7,17 @@ import re
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as f:
+        from_file = f.read()
+    with open(from_path, "r") as f:
+        template_file = f.read()
+    html_string = markdown_to_html_node(from_file).to_html()
+    title = extract_title(from_file)
+    template_file.replace("{{ Title }}", title)
+    template_file.replace("{{ Content }}", html_string)
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    with open(dest_path, "w") as f:
+        f.write(html_string)
 
 
 def extract_title(markdown):
@@ -40,6 +52,7 @@ def copy_dir_recursive(from_path, to_path):
 
 def main():
     copy_static()
+    generate_page('content/index.md', 'template.html', 'public/index.html')
 
 if __name__ == "__main__":
     main()
