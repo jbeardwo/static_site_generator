d0d8c7582ef7e0b1325ba562b562e2cca5037005
jbeardwo <jfwbeardwood@gmail.com>
2025-10-27 22:08:09 -0700

    added recursive page creation with that im done, skipping github.io deployment since i use that for other stuff

--- src/main.py
+++ src/main.py
@@ -5,11 +5,33 @@ import shutil
 import re
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for file in os.listdir(dir_path_content):
+        old_path = os.path.join(dir_path_content, file)
+        new_path = os.path.join(dest_dir_path, file)
+        if new_path.endswith(".md"):
+            new_path = new_path[:-3] + ".html"
+        if not os.path.isfile(old_path):
+            generate_pages_recursive(old_path, template_path, new_path)
+        else:
+            print(f"Generating page from {old_path} to {new_path} using {template_path}")
+            with open(old_path, "r") as f:
+                from_file = f.read()
+            with open(template_path) as f:
+                template_file = f.read()
+            html_string = markdown_to_html_node(from_file).to_html()
+            title = extract_title(from_file)
+            full_html = template_file.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+            os.makedirs(os.path.dirname(new_path), exist_ok=True)
+            with open(new_path, "w", encoding="utf-8") as f:
+                f.write(full_html)
+
+
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f:
         from_file = f.read()
-    with open(from_path, "r") as f:
+    with open(template_path, "r") as f:
         template_file = f.read()
     html_string = markdown_to_html_node(from_file).to_html()
     title = extract_title(from_file)
@@ -52,7 +74,7 @@ def copy_dir_recursive(from_path, to_path):
 
 def main():
     copy_static()
-    generate_page('content/index.md', 'template.html', 'public/index.html')
+    generate_pages_recursive('./content', 'template.html', './public')
 
 if __name__ == "__main__":
     main()
